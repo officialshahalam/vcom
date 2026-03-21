b7ba4ed7dc99669189c18a4012e636465578be54
Mohd Shahalam <officialshahalam855@gmail.com>
2026-03-21 22:53:02 +0530

    initial commit

---
+++ frontend/src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import "../global.css";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function TabLayout() {
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500 text-2xl">hka</Text>
+    </SafeAreaView>
+  );
+}
